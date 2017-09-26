c25a36013bb4fb7f37df04d5cdde48550e2be410
Josselin Buils <josselin.buils@gmail.com>
2017-09-27 01:18:19 +0200

    Improve projects command

--- src/app/terminal/executors/projects/projects.component.ts
+++ src/app/terminal/executors/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/toPromise';
 
@@ -15,6 +16,16 @@ export class ProjectsComponent implements OnInit {
   }
 
   async ngOnInit() {
-    this.repositories = (<any[]> await this.http.get('https://api.github.com/users/josselinbuils/repos').first().toPromise());
+    this.repositories = (<any[]> await this.http.get('https://api.github.com/users/josselinbuils/repos').first().toPromise())
+      .sort((a: any, b: any) => new Date(a.pushed_at).getTime() - new Date(b.pushed_at).getTime())
+      .map((repos: any) => {
+        return {
+          description: repos.description,
+          language: repos.language,
+          name: repos.name,
+          updated: moment(repos.pushed_at).fromNow(),
+          url: repos.html_url
+        };
+      });
   }
 }
