db2f5b20d21bff2e6adaa60ca0aa6f174d99d1a1
Josselin Buils <josselin.buils@gmail.com>
2017-09-27 20:11:44 +0200

    Add clear command

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,5 +1,6 @@
 import {
-  AfterContentInit, Component, ComponentFactoryResolver, HostListener, OnInit, Type, ViewChild, ViewContainerRef
+  AfterContentInit, Component, ComponentFactoryResolver, ComponentRef, HostListener, OnInit, Type, ViewChild,
+  ViewContainerRef
 } from '@angular/core';
 
 import { AboutComponent } from './executors/about/about.component';
@@ -15,6 +16,7 @@ enum KEY_CODE {
   BACK_SPACE = 8,
   DOWN = 40,
   ENTER = 13,
+  K = 75,
   UP = 38
 }
 
@@ -34,20 +36,22 @@ const executors = {
 export class TerminalComponent implements AfterContentInit, OnInit {
   @ViewChild('vc', {read: ViewContainerRef}) viewContainerRef: ViewContainerRef;
 
-  commandIndex: number;
-  commands: string[];
   userInput: string;
+
+  private commandIndex: number;
+  private commands: string[];
+  private components: ComponentRef<{}>[];
 
   constructor(private componentFactoryResolver: ComponentFactoryResolver) {
     this.userInput = '';
     this.commandIndex = 0;
     this.commands = [];
+    this.components = [];
   }
 
-  private loadComponent(component: Type<{}>, args: any[]) {
-    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(component);
-    const componentRef = this.viewContainerRef.createComponent(componentFactory);
-    (<Executor> componentRef.instance).args = args;
+  private clear() {
+    this.components.forEach((component: ComponentRef<{}>) => component.destroy());
+    this.components = [];
   }
 
   private exec(str: string) {
@@ -59,7 +63,9 @@ export class TerminalComponent implements AfterContentInit, OnInit {
       this.commands.push(str);
       this.commandIndex = this.commands.length;
 
-      if (executors[command]) {
+      if (command === 'clear') {
+        this.clear();
+      } else if (executors[command]) {
         this.loadComponent(executors[command], str.split(' ').slice(1));
       } else {
         this.loadComponent(BashErrorComponent, [command]);
@@ -67,35 +73,44 @@ export class TerminalComponent implements AfterContentInit, OnInit {
     }
   }
 
+  private loadComponent(component: Type<{}>, args: any[]) {
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(component);
+    const componentRef = this.viewContainerRef.createComponent(componentFactory);
+    (<Executor> componentRef.instance).args = args;
+    this.components.push(componentRef);
+  }
+
   @HostListener('window:keydown', ['$event'])
   keyboardListener(event: KeyboardEvent) {
-    if (event.key.length === 1) {
-      if (!event.altKey && !event.metaKey) {
+    if (!event.altKey && !event.metaKey) {
+      if (event.key.length === 1) {
         this.userInput += event.key;
+      } else {
+        switch (event.keyCode) {
+          case KEY_CODE.BACK_SPACE:
+            this.userInput = this.userInput.slice(0, -1);
+            break;
+          case KEY_CODE.ENTER:
+            this.exec(this.userInput);
+            this.userInput = '';
+            break;
+          case KEY_CODE.UP:
+            event.preventDefault();
+            if (this.commandIndex > 0) {
+              this.commandIndex--;
+              this.userInput = this.commands[this.commandIndex];
+            }
+            break;
+          case KEY_CODE.DOWN:
+            event.preventDefault();
+            if (this.commandIndex < (this.commands.length - 1)) {
+              this.commandIndex++;
+              this.userInput = this.commands[this.commandIndex];
+            }
+        }
       }
-    } else {
-      switch (event.keyCode) {
-        case KEY_CODE.BACK_SPACE:
-          this.userInput = this.userInput.slice(0, -1);
-          break;
-        case KEY_CODE.ENTER:
-          this.exec(this.userInput);
-          this.userInput = '';
-          break;
-        case KEY_CODE.UP:
-          event.preventDefault();
-          if (this.commandIndex > 0) {
-            this.commandIndex--;
-            this.userInput = this.commands[this.commandIndex];
-          }
-          break;
-        case KEY_CODE.DOWN:
-          event.preventDefault();
-          if (this.commandIndex < (this.commands.length - 1)) {
-            this.commandIndex++;
-            this.userInput = this.commands[this.commandIndex];
-          }
-      }
+    } else if (!event.altKey && event.metaKey && event.keyCode === KEY_CODE.K) {
+      this.clear();
     }
   }
 
