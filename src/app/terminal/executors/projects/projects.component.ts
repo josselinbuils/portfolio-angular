0356fd9d743f47eb9ddfd92d1271678567ab4ea9
Josselin BUILS <josselin.buils@gmail.com>
2017-10-07 00:36:14 +0200

    Update projects command cosmetic

--- src/app/terminal/executors/projects/projects.component.ts
+++ src/app/terminal/executors/projects/projects.component.ts
@@ -1,8 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import * as moment from 'moment';
 import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/toPromise';
+
+const projects = [
+  'MALVApi', 'MALViewer', 'MYOBotHub', 'MYOBotSite', 'pathFinding', 'pizzaDay', 'portfolio', 'reverseProxy', 'robot',
+  'teravia', 'youbiSMS'
+];
+
+const previews = {
+  pathFinding: {image: 'pathFinding.jpg'},
+  MYOBotHub: {image: 'MYOBotHub.jpg'},
+  MYOBotSite: {image: 'MYOBotSite.jpg'},
+  teravia: {image: 'teravia.jpg'},
+  youbiSMS: {image: 'youbiSMS.jpg'}
+};
 
 @Component({
   selector: 'app-projects',
@@ -16,11 +29,16 @@ export class ProjectsComponent implements OnInit {
   }
 
   async ngOnInit() {
-    this.repositories = (<any[]> await this.http.get('https://api.github.com/users/josselinbuils/repos').first().toPromise())
+    this.repositories = (<any[]> await this.http.get('https://api.github.com/users/josselinbuils/repos')
+      .first()
+      .toPromise())
+      .filter(repos => projects.indexOf(repos.name) !== -1)
       .sort((a: any, b: any) => new Date(a.pushed_at).getTime() - new Date(b.pushed_at).getTime())
       .map((repos: any) => {
+        const reposData = previews[repos.name];
         return {
           description: repos.description,
+          imageURL: reposData && reposData.image ? `assets/projects/${reposData.image}` : null,
           language: repos.language,
           name: repos.name,
           updated: moment(repos.pushed_at).fromNow(),
