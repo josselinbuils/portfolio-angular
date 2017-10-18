01008acc5e1ea6a4447b22951e082e180bbb4987
Josselin BUILS <josselin.buils@gmail.com>
2017-10-18 22:55:32 +0200

    Add selection no desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { AfterContentInit, Component, HostListener, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  AfterContentInit, Component, ElementRef, HostListener, Renderer2, ViewChild,
+  ViewContainerRef
+} from '@angular/core';
 import { WindowManagerService } from './window-manager.service';
 import { TerminalComponent } from './terminal/terminal.component';
 
@@ -8,20 +11,54 @@ import { TerminalComponent } from './terminal/terminal.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterContentInit {
+
+  @ViewChild('selection') selectionElementRef: ElementRef;
   @ViewChild('windows', {read: ViewContainerRef}) windowsViewContainerRef: ViewContainerRef;
 
-  constructor(private viewContainerRef: ViewContainerRef, private windowManagerService: WindowManagerService) {
+  private selection: HTMLElement;
+  private selectionStyle: any = null;
+
+  constructor(private renderer: Renderer2, private viewContainerRef: ViewContainerRef,
+              private windowManagerService: WindowManagerService) {
+  }
+
+  private startSelect(downEvent: MouseEvent): void {
+    downEvent.preventDefault();
+
+    const startX: number = downEvent.clientX;
+    const startY: number = downEvent.clientY;
+
+    this.selectionStyle = {
+      display: 'block',
+      width: 0,
+      height: 0
+    };
+
+    const cancelMouseMove: () => void = this.renderer.listen('window', 'mousemove', (moveEvent: MouseEvent) => {
+      this.selectionStyle['left.px'] = Math.min(startX, moveEvent.clientX);
+      this.selectionStyle['top.px'] = Math.min(startY, moveEvent.clientY);
+      this.selectionStyle['width.px'] = Math.abs(moveEvent.clientX - startX);
+      this.selectionStyle['height.px'] = Math.abs(moveEvent.clientY - startY);
+    });
+
+    const cancelMouseUp: () => void = this.renderer.listen('window', 'mouseup', () => {
+      this.selectionStyle = null;
+      cancelMouseMove();
+      cancelMouseUp();
+    });
   }
 
   @HostListener('mousedown', ['$event'])
   eventListener(event: MouseEvent): void {
     if (event.target === this.viewContainerRef.element.nativeElement) {
       this.windowManagerService.unselectAllWindows();
+      this.startSelect(event);
     }
   }
 
   ngAfterContentInit() {
     this.windowManagerService.setViewContainerRef(this.windowsViewContainerRef);
     this.windowManagerService.openWindow(TerminalComponent);
+    this.selection = this.selectionElementRef.nativeElement;
   }
 }
