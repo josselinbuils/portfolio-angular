4ef95a9956bdb866ab7459d627322822ff48a90f
Josselin BUILS <josselin.buils@gmail.com>
2017-12-19 22:14:23 +0100

    Add cache to canvas renderer

--- src/app/apps/dicom-viewer/renderer/canvas/canvas-renderer.ts
+++ src/app/apps/dicom-viewer/renderer/canvas/canvas-renderer.ts
@@ -6,6 +6,7 @@ export class CanvasRenderer implements Renderer {
   private context: CanvasRenderingContext2D;
   private lut: any;
   private renderingContext: CanvasRenderingContext2D;
+  private renderingProperties: any;
 
   constructor(canvas: HTMLCanvasElement) {
     this.context = canvas.getContext('2d');
@@ -40,58 +41,69 @@ export class CanvasRenderer implements Renderer {
     const imageWidth: number = Math.round(width * renderZoom);
     const imageHeight: number = Math.round(height * renderZoom);
 
-    const length: number = imageWidth * imageHeight * 4;
+    const renderingProperties: any = {
+      imageWidth,
+      imageHeight,
+      windowWidth: viewport.windowWidth,
+      windowLevel: viewport.windowLevel,
+    };
 
-    if (length > 0) {
-      const leftLimit: number = Math.floor(viewport.windowLevel - viewport.windowWidth / 2);
-      const rightLimit: number = Math.floor(viewport.windowLevel + viewport.windowWidth / 2);
-      const imageData: Uint8ClampedArray = new Uint8ClampedArray(length);
-      let dataIndex: number = 0;
+    if (JSON.stringify(renderingProperties) !== JSON.stringify(this.renderingProperties)) {
+      const length: number = imageWidth * imageHeight * 4;
 
-      for (let y: number = 0; y < imageWidth; y++) {
-        for (let x: number = 0; x < imageHeight; x++) {
-          const pixelDataIndex: number = Math.round(y / renderZoom) * width + Math.round(x / renderZoom);
-          const rawValue: number = pixelData[pixelDataIndex] * rescaleSlope + rescaleIntercept;
-          let intensity: number = 0;
+      if (length > 0) {
+        const leftLimit: number = Math.floor(viewport.windowLevel - viewport.windowWidth / 2);
+        const rightLimit: number = Math.floor(viewport.windowLevel + viewport.windowWidth / 2);
+        const imageData: Uint8ClampedArray = new Uint8ClampedArray(length);
+        let dataIndex: number = 0;
 
-          if (rawValue >= rightLimit) {
-            intensity = 255;
-          } else if (rawValue > leftLimit) {
-            intensity = this.lut.table[rawValue - leftLimit];
+        for (let y: number = 0; y < imageWidth; y++) {
+          for (let x: number = 0; x < imageHeight; x++) {
+            const pixelDataIndex: number = Math.round(y / renderZoom) * width + Math.round(x / renderZoom);
+            const rawValue: number = pixelData[pixelDataIndex] * rescaleSlope + rescaleIntercept;
+            let intensity: number = 0;
+
+            if (rawValue >= rightLimit) {
+              intensity = 255;
+            } else if (rawValue > leftLimit) {
+              intensity = this.lut.table[rawValue - leftLimit];
+            }
+
+            imageData[dataIndex++] = intensity;
+            imageData[dataIndex++] = intensity;
+            imageData[dataIndex++] = intensity;
+            imageData[dataIndex++] = 255;
           }
-
-          imageData[dataIndex++] = intensity;
-          imageData[dataIndex++] = intensity;
-          imageData[dataIndex++] = intensity;
-          imageData[dataIndex++] = 255;
         }
+
+        let imageDataInstance: ImageData;
+
+        try {
+          imageDataInstance = new ImageData(imageData, imageWidth, imageHeight);
+        } catch (e) {
+          imageDataInstance = this.context.createImageData(imageWidth, imageHeight);
+          imageDataInstance.data.set(imageData);
+        }
+
+        const renderingCanvas: HTMLCanvasElement = this.renderingContext.canvas;
+        renderingCanvas.width = imageWidth;
+        renderingCanvas.height = imageHeight;
+        this.renderingContext.putImageData(imageDataInstance, 0, 0);
       }
 
-      let imageDataInstance: ImageData;
-
-      try {
-        imageDataInstance = new ImageData(imageData, imageWidth, imageHeight);
-      } catch (e) {
-        imageDataInstance = this.context.createImageData(imageWidth, imageHeight);
-        imageDataInstance.data.set(imageData);
-      }
-
-      const renderingCanvas: HTMLCanvasElement = this.renderingContext.canvas;
-      renderingCanvas.width = imageWidth;
-      renderingCanvas.height = imageHeight;
-      this.renderingContext.putImageData(imageDataInstance, 0, 0);
-
-      const newWidth: number = Math.round(imageWidth * zoom);
-      const newHeight: number = Math.round(imageHeight * zoom);
-
-      const newX: number = Math.round((viewport.width - newWidth) / 2 + viewport.deltaX * viewport.width);
-      const newY: number = Math.round((viewport.height - newHeight) / 2 + viewport.deltaY * viewport.height);
-
-      (<any> this.context).msImageSmoothingEnabled = false;
-      this.context.imageSmoothingEnabled = false;
-
-      this.context.drawImage(renderingCanvas, newX, newY, newWidth, newHeight);
+      this.renderingProperties = renderingProperties;
     }
+
+    const newWidth: number = Math.round(imageWidth * zoom);
+    const newHeight: number = Math.round(imageHeight * zoom);
+
+    const newX: number = Math.round((viewport.width - newWidth) / 2 + viewport.deltaX * viewport.width);
+    const newY: number = Math.round((viewport.height - newHeight) / 2 + viewport.deltaY * viewport.height);
+
+    (<any> this.context).msImageSmoothingEnabled = false;
+    this.context.imageSmoothingEnabled = false;
+
+    this.context.drawImage(this.renderingContext.canvas, newX, newY, newWidth, newHeight);
   }
 
   resize(viewport: Viewport): void {
