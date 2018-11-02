2a1b70e4e97251fa1fc8860b820ddb23a0c6ab75
Josselin BUILS <josselin.buils@gmail.com>
2018-11-02 18:22:10 +0100

    Cosmetic

--- src/app/apps/dicom-viewer/renderer/emscripten/wrappers/wasm-pre.js
+++
@@ -1,6 +0,0 @@
-function WasmRenderer() {
-  return new Promise(function (resolve) {
-    var Module = {};
-    Module['onRuntimeInitialized'] = function () { resolve(Module); };
-    Module['print'] = Module['printErr'] = function () {};
-    Module['wasmBinaryFile'] = '/assets/wasm-renderer.wasm';
