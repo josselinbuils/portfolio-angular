682f8edf69ef07f56e1b5c65541c901823560198
Josselin BUILS <josselin.buils@gmail.com>
2018-11-11 18:28:33 +0100

    Enables interpolation with webgl renderer

--- src/app/apps/dicom-viewer/renderer/webgl/webgl-renderer.ts
+++ src/app/apps/dicom-viewer/renderer/webgl/webgl-renderer.ts
@@ -127,8 +127,8 @@ export class WebGLRenderer implements Renderer {
     // Set the parameters so we can render any size image.
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
     const { height, imageFormat, width } = image;
     const format = getTextureFormat(gl, imageFormat);
