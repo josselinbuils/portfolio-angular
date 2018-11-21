6aec0065ae6629f55aa6102d6134348477ef1490
Josselin BUILS <josselin.buils@gmail.com>
2018-11-21 11:33:00 +0100

    Uses linear search when binary search does not work

--- src/app/apps/dicom-viewer/helpers/camera-helpers.ts
+++ src/app/apps/dicom-viewer/helpers/camera-helpers.ts
@@ -4,51 +4,76 @@ import { Camera, DicomDataset, Frame } from '../models';
 
 const DEBUG = false;
 
-// TODO improve this
 export function findFrame(dataset: DicomDataset, camera: Camera): Frame {
-  if (DEBUG) { console.time('binary'); }
+  try {
+    return findFrameBinary(dataset, camera);
+  } catch (error) {
+    console.warn('Unable to find frame using binary search, use linear search instead');
+    return findFrameLinear(dataset, camera);
+  }
+}
+
+// TODO improve this
+function findFrameBinary(dataset: DicomDataset, camera: Camera): Frame {
+  if (DEBUG) { console.time(); }
+
   const frames = dataset.frames;
+
+  if (frames.length === 1) {
+    return frames[0];
+  }
+
   const direction = camera.getDirection();
   let index = Math.floor(dataset.frames.length / 2);
   let left = 0;
   let right = frames.length - 1;
 
-  if (right === 0) {
-    return frames[0];
-  }
-
-  while (right !== left) {
-    const previousFrameDistance = math.chain(frames[index - 1].imageCenter)
-      .subtract(camera.lookPoint)
-      .dot(direction)
-      .abs()
-      .done();
+  while (true) {
+    const previousFrameDistance = getDistanceBetweenPoints(frames[index - 1].imageCenter, camera.lookPoint, direction);
 
     if (previousFrameDistance < Number.EPSILON) {
-      if (DEBUG) { console.timeEnd('binary'); }
+      if (DEBUG) { console.timeEnd(); }
       return frames[index - 1];
     }
 
-    const frameDistance = math.chain(frames[index].imageCenter)
-      .subtract(camera.lookPoint)
-      .dot(direction)
-      .abs()
-      .done();
+    const frameDistance = getDistanceBetweenPoints(frames[index].imageCenter, camera.lookPoint, direction);
 
     if (frameDistance < Number.EPSILON) {
-      if (DEBUG) { console.timeEnd('binary'); }
+      if (DEBUG) { console.timeEnd(); }
       return frames[index];
     }
 
     const dir = previousFrameDistance < frameDistance ? -1 : 1;
 
-    if (dir < 0) {
+    if (dir < 0 && right !== index) {
       right = index;
-    } else {
+    } else if (left !== index) {
       left = index;
+    } else {
+      console.log(camera.lookPoint);
+      console.log(index - 1, frames[index - 1].imageCenter, previousFrameDistance);
+      console.log(index, frames[index].imageCenter, frameDistance);
+      break;
     }
 
     index += Math.ceil((right - left) / 2) * dir;
   }
   throw new Error('Unable to find frame');
+}
+
+function findFrameLinear(dataset: DicomDataset, camera: Camera): Frame {
+  const direction = camera.getDirection();
+
+  for (const frame of dataset.frames) {
+    const distance = getDistanceBetweenPoints(frame.imageCenter, camera.lookPoint, direction);
+
+    if (distance < Number.EPSILON) {
+      return frame;
+    }
+  }
+  throw new Error('Unable to find frame');
+}
+
+function getDistanceBetweenPoints(pointA: number[], pointB: number[], axe: number[]): number {
+  return math.chain(pointA).subtract(pointB).dot(axe).abs().done();
 }
