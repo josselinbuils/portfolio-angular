47e61f71b1c030593ed55c674e2257ecd3190bbc
Josselin BUILS <josselin.buils@gmail.com>
2019-02-24 11:35:17 +0100

    Removes corejs polyfills
    - Use polyfill.io instead
    - Removed unused dependencies
    - Edited JSON schema in index.html

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -14,44 +14,5 @@
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
 
-/***************************************************************************************************
- * BROWSER POLYFILLS
- */
-
-import 'core-js/es6/array';
-import 'core-js/es6/date';
-import 'core-js/es6/function';
-import 'core-js/es6/map';
-import 'core-js/es6/math';
-import 'core-js/es6/number';
-import 'core-js/es6/object';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/reflect';
-import 'core-js/es6/regexp';
-import 'core-js/es6/set';
-import 'core-js/es6/string';
-import 'core-js/es6/symbol';
-import 'core-js/es6/weak-map';
-import 'core-js/es7/array';
+// Needed by Angular, included with Angular CLI.
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-
-if (Element.prototype.matches === undefined) {
-  Element.prototype.matches = [
-    (Element as any).prototype.matchesSelector,
-    (Element as any).prototype.mozMatchesSelector,
-    (Element as any).prototype.msMatchesSelector,
-    (Element as any).prototype.oMatchesSelector,
-    (Element as any).prototype.webkitMatchesSelector,
-    function (s: string): boolean {
-      const matches = (this.document || this.ownerDocument).querySelectorAll(s);
-      let i = matches.length;
-      while (--i >= 0 && matches.item(i) !== this) {}
-      return i > -1;
-    },
-  ].find(method => typeof method === 'function');
-}
