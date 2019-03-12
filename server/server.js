286af0bd0bd7e763af2717112b6877d647b8f1a3
Josselin BUILS <josselin.buils@gmail.com>
2019-03-12 20:05:33 +0100

    Removes security as it is handled by reverse proxy

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
-const contentLength = require('express-content-length-validator');
 const express = require('express');
-const helmet = require('helmet');
 
-const { ENV_DEV, MAX_CONTENT_LENGTH, PORT } = require('./constants.json');
+const { ENV_DEV, PORT } = require('./constants.json');
 const Logger = require('./logger');
 const Router = require('./router');
 
 const ENV = process.env.NODE_ENV || ENV_DEV;
 
 const app = express();
-
-app.use(helmet());
-app.use(contentLength.validateMax({ max: MAX_CONTENT_LENGTH }));
 
 Logger.info(`Start portfolio server in ${ENV} mode`);
 
